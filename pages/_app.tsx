2bbdd64519857ea9c2246e0699af6dce5b80d9b0
Jesper Persson <jesper.persson_96@hotmail.com>
2022-08-24 14:20:04 +0200

    More CSS fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      theme={{
+        fontFamily: "'Poppins', sans-serif",
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
