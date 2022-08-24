7ca1ea2168ff7a53d1cf75eb819d09c1f81b31ed
Jesper Persson <jesper.persson_96@hotmail.com>
2022-08-24 14:19:50 +0200

    CSS fixes

--- pages/table.tsx
+++ pages/table.tsx
@@ -2,37 +2,40 @@ import { Team } from "types";
 import { fetchTable } from "../lib/parser/table";
 import { Table, Container } from '@mantine/core';
 
-export default function Home({ table }: Team[]) {
+export default function TableStanding({ table }: Team[]) {
   const rows = table.map((team) => (
     <tr key={team.name}>
-      <td style={{borderBottom: "none"}}>{team.position}</td>
-      <td style={{borderBottom: "none"}}>{team.name}</td>
-      <td style={{textAlign: "center", borderBottom: "none"}}>{team.playedGames}</td>
-      <td style={{textAlign: "center", borderBottom: "none"}}>{team.wins}</td>
-      <td style={{textAlign: "center", borderBottom: "none"}}>{team.draws}</td>
-      <td style={{textAlign: "center", borderBottom: "none"}}>{team.losses}</td>
-      <td style={{textAlign: "center", borderBottom: "none"}}>{team.goalDifferences}</td>
-      <td style={{textAlign: "center", borderBottom: "none"}}>{team.points}</td>
+      <td className="no-border info"><div className="qualification"></div></td>
+      <td className="no-border">{team.position}</td>
+      <td className="no-border">{team.name}</td>
+      <td className="no-border center">{team.playedGames}</td>
+      <td className="no-border center">{team.wins}</td>
+      <td className="no-border center">{team.draws}</td>
+      <td className="no-border center">{team.losses}</td>
+      <td className="no-border center">{team.goalDifferences}</td>
+      <td className="no-border center">{team.points}</td>
     </tr>
   ));
 
   return (
     <Container size="sm" px="sm" style={{background: "white", borderRadius: "8px", border: "0.5px solid lightgray", padding: "0"}}>
-      <Table striped style={{fontFamily: "'Poppins', sans-serif"}} fontSize="md" verticalSpacing="sm">
+      <Table striped fontSize="md" verticalSpacing="sm">
         <thead>
           <tr>
-            <th style={{borderBottom: "none"}}>#</th>
-            <th style={{borderBottom: "none"}}> Klubb</th>
-            <th style={{textAlign: "center", borderBottom: "none"}}>SM</th>
-            <th style={{textAlign: "center", borderBottom: "none"}}>V</th>
-            <th style={{textAlign: "center", borderBottom: "none"}}>O</th>
-            <th style={{textAlign: "center", borderBottom: "none"}}>F</th>
-            <th style={{textAlign: "center", borderBottom: "none"}}>+/-</th>
-            <th style={{textAlign: "center", borderBottom: "none"}}>P</th>
+            <th></th>
+            <th>#</th>
+            <th> Klubb</th>
+            <th className="center">SM</th>
+            <th className="center">V</th>
+            <th className="center">O</th>
+            <th className="center">F</th>
+            <th className="center">+/-</th>
+            <th className="center">P</th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
       </Table>
+
     </Container>
   );
 }
